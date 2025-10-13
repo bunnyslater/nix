80dcf24cacf02b8b7e575d88cd4646b9ba82c3a8
bunnyslater <211062560+bunnyslater@users.noreply.github.com>
2025-10-13 15:12:28 +0100

    Add xdg.mimeApps, Konsole configurations and include keybinds in repo

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -28,6 +28,76 @@ in {
     text = "{ allowUnfree = true; }";
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = [ "chromium-browser.desktop" ];
+      "audio/aac" = [ "vlc.desktop" ];
+      "audio/mp4" = [ "vlc.desktop" ];
+      "audio/mpeg" = [ "vlc.desktop" ];
+      "audio/mpegurl" = [ "vlc.desktop" ];
+      "audio/ogg" = [ "vlc.desktop" ];
+      "audio/vnd.rn-realaudio" = [ "vlc.desktop" ];
+      "audio/vorbis" = [ "vlc.desktop" ];
+      "audio/x-flac" = [ "vlc.desktop" ];
+      "audio/x-mp3" = [ "vlc.desktop" ];
+      "audio/x-mpegurl" = [ "vlc.desktop" ];
+      "audio/x-ms-wma" = [ "vlc.desktop" ];
+      "audio/x-musepack" = [ "vlc.desktop" ];
+      "audio/x-oggflac" = [ "vlc.desktop" ];
+      "audio/x-pn-realaudio" = [ "vlc.desktop" ];
+      "audio/x-scpls" = [ "vlc.desktop" ];
+      "audio/x-speex" = [ "vlc.desktop" ];
+      "audio/x-vorbis" = [ "vlc.desktop" ];
+      "audio/x-vorbis+ogg" = [ "vlc.desktop" ];
+      "audio/x-wav" = [ "vlc.desktop" ];
+      "image/bmp" = [ "org.gnome.eog.desktop" ];
+      "image/jpeg" = [ "org.gnome.eog.desktop" ];
+      "image/png" = [ "org.gnome.eog.desktop" ];
+      "image/webp" = [ "org.gnome.eog.desktop" ];
+      "image/x-icns" = [ "org.gnome.eog.desktop" ];
+      "text/plain" = [ "code.desktop" ];
+      "video/mp4" = [ "vlc.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/mailto" = [ "firefox.desktop" ];
+    };
+    defaultApplications = {
+      "application/pdf" = [ "chromium-browser.desktop" ];
+      "audio/aac" = [ "vlc.desktop" ];
+      "audio/mp4" = [ "vlc.desktop" ];
+      "audio/mpeg" = [ "vlc.desktop" ];
+      "audio/mpegurl" = [ "vlc.desktop" ];
+      "audio/ogg" = [ "vlc.desktop" ];
+      "audio/vnd.rn-realaudio" = [ "vlc.desktop" ];
+      "audio/vorbis" = [ "vlc.desktop" ];
+      "audio/x-flac" = [ "vlc.desktop" ];
+      "audio/x-mp3" = [ "vlc.desktop" ];
+      "audio/x-mpegurl" = [ "vlc.desktop" ];
+      "audio/x-ms-wma" = [ "vlc.desktop" ];
+      "audio/x-musepack" = [ "vlc.desktop" ];
+      "audio/x-oggflac" = [ "vlc.desktop" ];
+      "audio/x-pn-realaudio" = [ "vlc.desktop" ];
+      "audio/x-scpls" = [ "vlc.desktop" ];
+      "audio/x-speex" = [ "vlc.desktop" ];
+      "audio/x-vorbis" = [ "vlc.desktop" ];
+      "audio/x-vorbis+ogg" = [ "vlc.desktop" ];
+      "audio/x-wav" = [ "vlc.desktop" ];
+      "image/bmp" = [ "org.gnome.eog.desktop" ];
+      "image/jpeg" = [ "org.gnome.eog.desktop" ];
+      "image/png" = [ "org.gnome.eog.desktop" ];
+      "image/webp" = [ "org.gnome.eog.desktop" ];
+      "image/x-icns" = [ "org.gnome.eog.desktop" ];
+      "text/plain" = [ "code.desktop" ];
+      "video/mp4" = [ "vlc.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/mailto" = [ "firefox.desktop" ];
+      "x-scheme-handler/x-github-client" = [ "github-desktop.desktop" ];
+      "x-scheme-handler/x-github-desktop-auth" = [ "github-desktop.desktop" ];
+      };
+  };
+
   programs = {
     fish = {
       enable = true;
@@ -119,7 +189,121 @@ in {
       }
     ];
     };
+  };
 
+  home.file.".local/share/konsole/Breeze.colorscheme" = {
+    text = ''
+      [Background]
+      Color=20,29,42
 
+      [BackgroundFaint]
+      Color=49,54,59
+
+      [BackgroundIntense]
+      Color=0,0,0
+
+      [Color0]
+      Color=35,38,39
+
+      [Color0Faint]
+      Color=49,54,59
+
+      [Color0Intense]
+      Color=127,140,141
+
+      [Color1]
+      Color=237,21,21
+
+      [Color1Faint]
+      Color=120,50,40
+
+      [Color1Intense]
+      Color=192,57,43
+
+      [Color2]
+      Color=17,209,22
+
+      [Color2Faint]
+      Color=23,162,98
+
+      [Color2Intense]
+      Color=28,220,154
+
+      [Color3]
+      Color=246,116,0
+
+      [Color3Faint]
+      Color=182,86,25
+
+      [Color3Intense]
+      Color=253,188,75
+
+      [Color4]
+      Color=29,153,243
+
+      [Color4Faint]
+      Color=27,102,143
+
+      [Color4Intense]
+      Color=61,174,233
+
+      [Color5]
+      Color=155,89,182
+
+      [Color5Faint]
+      Color=97,74,115
+
+      [Color5Intense]
+      Color=142,68,173
+
+      [Color6]
+      Color=26,188,156
+
+      [Color6Faint]
+      Color=24,108,96
+
+      [Color6Intense]
+      Color=22,160,133
+
+      [Color7]
+      Color=252,252,252
+
+      [Color7Faint]
+      Color=99,104,109
+
+      [Color7Intense]
+      Color=255,255,255
+
+      [Foreground]
+      Color=252,252,252
+
+      [ForegroundFaint]
+      Color=239,240,241
+
+      [ForegroundIntense]
+      Color=61,174,233
+
+      [General]
+      Anchor=0.5,0.5
+      Blur=false
+      ColorRandomization=false
+      Description=Breeze
+      FillStyle=Tile
+      Opacity=1
+      Wallpaper=
+      WallpaperFlipType=NoFlip
+      WallpaperOpacity=1 '';
+  };
+
+  home.file.".local/share/konsole/Profile\ 1.profile" = {
+    text = ''
+      [Appearance]
+      ColorScheme=Breeze
+      Font=Hack,12,-1,7,400,0,0,0,0,0,0,0,0,0,0,1
+
+      [General]
+      Name=Profile 1
+      Parent=FALLBACK/
+    '';
   };
 }
