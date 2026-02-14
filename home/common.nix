067187be9da57855c62c39dbc854f0644830d073
bunnyslater <211062560+bunnyslater@users.noreply.github.com>
2026-02-14 17:21:04 +0000

    Configure Git

--- home/common.nix
+++ home/common.nix
@@ -92,6 +92,16 @@
         end
       '';
     };
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name  = "bunnyslater";
+          email = "211062560+bunnyslater@users.noreply.github.com";
+        };
+        init.defaultBranch = "main";
+      };
+    };
     hyfetch = {
       enable = true;
       settings = {
