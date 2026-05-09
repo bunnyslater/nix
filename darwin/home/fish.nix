0c85a9378f588bfe7682f7d66737d1cdbaac6750
bunnyslater <211062560+bunnyslater@users.noreply.github.com>
2026-05-09 17:14:27 +0100

    darwin: futher fish configuration

--- darwin/home/fish.nix
+++ darwin/home/fish.nix
@@ -9,12 +9,17 @@
       fish_add_path /nix/var/nix/profiles/default/bin
       fish_add_path /opt/homebrew/bin
       fish_add_path ~/.local/bin
+
+      export OPENCODE_API_KEY="op://Personal/Opencode API Key/credential"
+      export EDITOR="zed"
     '';
 
     shellAliases = {
       s = "sudo darwin-rebuild switch --flake ~/dev/nix#${hostname}";
       fastfetch = "hyfetch";
       tidyup = "nix-collect-garbage -d";
+      omp = "op run -- omp";
+      pi = "op run -- omp";
     };
 
     interactiveShellInit = ''
